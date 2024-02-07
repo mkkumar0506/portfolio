2b60185058431cb3a079a3e1cfe44168159cc962
mkkumar0506 <53848169+mkkumar0506@users.noreply.github.com>
2024-02-07 16:23:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text",{
+    strings:[" Java Developer" , "Web developer ", "Sotware Tester", "Digital Marketing Executive"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+})
