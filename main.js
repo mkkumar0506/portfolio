bbafb721cbf462230c32ff5f943a60cb2a0e6694
mkkumar0506 <53848169+mkkumar0506@users.noreply.github.com>
2024-02-29 15:25:30 +0530

    main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings:[" Java Developer" , "Web developer ", "Sotware Tester", "Digital Marketing Executive"],
+    strings:[" Java Developer" , "Web developer ", "Dot Net Developer", "Digital Marketing Executive"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
